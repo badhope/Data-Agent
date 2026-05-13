a210bbec22a2c7772dc1fcced09fa39547adc5ec
badhope <badhope@users.noreply.github.com>
2026-05-13 11:01:08 +0000

    chore: remove chat_app directory

--- chat_app/app/agents/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Agents module for the chat application."""
-
-from .agent_manager import AgentManager, get_agent_manager, create_agent
-
-__all__ = [
-    'AgentManager',
-    'get_agent_manager',
-    'create_agent',
-]
